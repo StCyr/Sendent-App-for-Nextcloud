1b643cbbd172c7156afa375ffd470ce50e01edc9
Léon van de Broek <leonvandebroek@me.com>
2021-08-10 20:51:05 +0200

    release(1.2.6)

--- lib/Controller/StatusApiController.php
+++ lib/Controller/StatusApiController.php
@@ -3,6 +3,7 @@
 namespace OCA\Sendent\Controller;
 
 use OCP\IRequest;
+use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\ApiController;
 use OCA\Sendent\Db\Status;
@@ -31,7 +32,7 @@ class StatusApiController extends ApiController {
 	public function index() {
 		$statusobj = new Status();
 		$statusobj->app = "sendent";
-		$statusobj->version = "1.2.5";
+		$statusobj->version = "1.2.6";
 		$statusobj->currentuserid = $this->userId;
 		if($this->licensemanager->isLicenseCheckNeeded())
 		{
@@ -40,7 +41,7 @@ class StatusApiController extends ApiController {
 			$result = $this->licenseservice->findAll();
 			if (isset($result) && $result !== null && $result !== false) {
 				if (is_array($result) && count($result) > 0 
-				&& $result[0]->getLevel() != "Error_clear" && $result[0]->getLevel() != "Error_incomplete") {
+				&& $result[0]->getLevel() != "Error_clear" && $result[0]->getLevel() != "Error_incomplete" && $result[0]->getLevel() != "none") {
 					$statusobj->datelicenseend = $result[0]->getDatelicenseend();
 					$statusobj->maxusers = $result[0]->getMaxusers();
 					$statusobj->dategraceperiodend = $result[0]->getDategraceperiodend();
@@ -59,17 +60,30 @@ class StatusApiController extends ApiController {
 					}
 					$statusobj->licenseaction = $status;
 				}
-			}
-			else{
-					$statusobj->datelicenseend = date_create("2021-01-01");
+				else{
+					$statusobj->datelicenseend = date_format(date_create("2021-01-01"),"Y-m-d H:i:s");
 					$statusobj->maxusers = 0;
-					$statusobj->dategraceperiodend = date_create("2021-01-01");
+					$statusobj->dategraceperiodend = date_format(date_create("2021-01-01"),"Y-m-d H:i:s");
 					$statusobj->maxusersgrace = 0;
 					$statusobj->currentusers = 0;
 					$statusobj->validlicense = false;
 					$status = "";
-						$status = "Free";
+					$status = "Free";
 					$statusobj->licenseaction = $status;
+					return new DataResponse($statusobj, Http::STATUS_NOT_FOUND);
+			}
+			}
+			else{
+					$statusobj->datelicenseend = date_format(date_create("2021-01-01"),"Y-m-d H:i:s");
+					$statusobj->maxusers = 0;
+					$statusobj->dategraceperiodend = date_format(date_create("2021-01-01"),"Y-m-d H:i:s");
+					$statusobj->maxusersgrace = 0;
+					$statusobj->currentusers = 0;
+					$statusobj->validlicense = false;
+					$status = "";
+					$status = "Free";
+					$statusobj->licenseaction = $status;
+					return new DataResponse($statusobj, Http::STATUS_NOT_FOUND);
 			}
 		
 		
