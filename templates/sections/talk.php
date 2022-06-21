6704bb34b80fee0c3c2b0efbf0dee59faa1722cc
Léon van de Broek <leonvandebroek@me.com>
2022-06-21 23:08:42 +0200

    feature(activate-talk-setting-brought-back)

--- templates/sections/talk.php
+++ templates/sections/talk.php
@@ -6,6 +6,26 @@
         <div class="settingkeyvalue">
             <label>
                 <span class="templatesettingkeyname">
+                <?php p($l->t('Activate')); ?> Talk
+                </span>
+            </label>
+            <div class="status-error icon-error error hidden"></div>
+            <div class="status-ok icon-checkmark ok hidden"></div>
+            <select class="settingkeyvalueinput" type="select" name="settingkeyvalueinput" id="talkenabled">
+                <option value="True"><?php p($l->t('Enabled')); ?></option>
+                <option selected value="False"><?php p($l->t('Disabled')); ?></option>
+            </select>
+            <input type="hidden" name="settingkeyname" value="talkenabled">
+            <input type="hidden" name="settingkeytemplateid" value="0">
+            <input type="hidden" name="settinggroupid" value="0">
+            <input type="hidden" name="settingkeykey" value="202">
+            <input type="hidden" name="settingkeyid" value="202">
+        </div>
+    </div>
+    <div class="personal-settings-setting-box">
+        <div class="settingkeyvalue">
+            <label>
+                <span class="templatesettingkeyname">
                     <?php p($l->t('Generate password for meetings')); ?>
                 </span>
             </label>
