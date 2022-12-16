a33de683cb4b75093a3ed27ff46f50413319a88c
Cyrille Bollu <cyrille@debian-BULLSEYE-live-builder-AMD64>
2022-12-16 23:55:13 +0100

    Removes superfluous space characters
    Signed-off-by: Cyrille Bollu <cyrille@debian-BULLSEYE-live-builder-AMD64>

--- templates/sections/groupsManagement.php
+++ templates/sections/groupsManagement.php
@@ -6,13 +6,13 @@
 	<div style="display: flex;">
 		<ul id="ncGroups" class="connectedSortable" style="min-width:270px; margin-right: 5px; padding: 2px; border: 1px solid #eee; min-height: 270px;">
 			<?php foreach($_['ncGroups'] as $group) { ?>
-				<li class="ui-state-default" style="padding-left: 2px;"> <?php p($group); ?> </li>
+				<li class="ui-state-default" style="padding-left: 2px;"><?php p($group); ?></li>
 			<?php } ?>
 		</ul>
  
 		<ul id="sendentGroups" class="connectedSortable" style="min-width:270px; margin-right: 5px; padding: 2px; border: 1px solid #eee; min-height: 270px;">
 			<?php foreach($_['sendentGroups'] as $group) { ?>
-				<li class="ui-state-default" style="padding-left: 2px;"> <?php p($group); ?> </li>
+				<li class="ui-state-default" style="padding-left: 2px;"><?php p($group); ?></li>
 			<?php } ?>
 		</ul>
 	</div>
