e5bb122695fde8e9c1eb04f8b81cefe15aaaf404
Cyrille Bollu <cyrille@debian-BULLSEYE-live-builder-AMD64>
2023-02-06 16:54:50 +0100

    Removes constants that are not used anymore since we are getting the capabilties using the ocs api (from the frontend)
    Signed-off-by: Cyrille Bollu <cyrille@debian-BULLSEYE-live-builder-AMD64>

--- lib/Constants.php
+++ lib/Constants.php
@@ -6,6 +6,4 @@ class Constants {
 	public const CONFIG_UPLOAD_TAG = 'tag:upload';
 	public const CONFIG_REMOVED_TAG = 'tag:removed';
 	public const CONFIG_EXPIRED_TAG = 'tag:expired';
-	public const APPS_REQUIRED = ["core", "files", "dav", "ocm", "files_sharing", "password_policy", "theming"];
-	public const APPS_RECOMMENDED = ["activity", "talk"];
 }
